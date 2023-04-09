14087f76e0e025307dd62545d9002b39b65ca571
simon9141 <simonotieno9141@gmail.com>
2023-04-09 13:01:56 +0300

    added comments and updated the test folder

--- blogger/lib/blogger.ex
+++ blogger/lib/blogger.ex
@@ -1,22 +1,22 @@
 defmodule Blogger do
   use HTTPoison.Base
 
-
+  #function to change String input to upper case
   def to_upper_case(strings) do
     Enum.map(strings, &String.upcase/1)
   end
 
-  
+  #function to fetch blog from a given url using HTTPoison
   def fetch_blog(url) do
     response = get(url)
 
     case response do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         IO.puts("Successfully fetched blog post!")
-        body
+        {:ok, body}
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts("Unexpected response code: #{404}")
-
+        {:error, "Blog not found"}
     end
   end
 
--- blogger/test/blogger_test.exs
+++ blogger/test/blogger_test.exs
@@ -2,7 +2,9 @@ defmodule BloggerTest do
   use ExUnit.Case
   doctest Blogger
 
-  test "greets the world" do
-    assert Blogger.hello() == :world
+
+  test "fetches a blog post from URL" do
+    {:ok, body} = Blogger.fetch_blog("https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91")
+    assert String.contains?(body, "Quick connect to your Amazon ec2 linux instance through the command line")
   end
 end
